e3dabf6d898f092c7be08799e46db854c790ee3a
Hejin0-0 <lovenstein0624@gmail.com>
2024-03-07 16:49:50 +0900

    Flappy_Bird_2day

--- Flappy_Bird/src/index.js
+++ Flappy_Bird/src/index.js
@@ -1,57 +1,31 @@
 /* https://phaser.io/ */
 
 import Phaser from "phaser";
+import PlayScene from "./scenes/PlayScene";
+
+const WIDTH = 800;
+const HEIGHT = 600;
+const BIRD_POSITION = { x: WIDTH * 0.1, y: HEIGHT / 2 };
+
+const SHARED_CONFIG = {
+	width: WIDTH,
+	height: HEIGHT,
+	startPosition: BIRD_POSITION,
+};
 
 const config = {
 	// WebGL (Web graphics librar) JS for rendering 2D and 3D graphics
 	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
+	...SHARED_CONFIG,
 	physics: {
 		// Arcade physics plugin, manages physics simulation
 		default: "arcade",
 		arcade: {
-			gravity: { y: 400 },
+			// gravity: { y: 400 },
 			debug: true,
 		},
 	},
-	scene: {
-		preload,
-		create,
-		update,
-	},
+	scene: [new PlayScene(SHARED_CONFIG)],
 };
-
-// Loading assets, such as images, music, animations ...
-function preload() {
-	// this context - scene
-	// contains functions and properties we can use
-	this.load.image("sky", "assets/sky.png");
-	this.load.image("bird", "assets/bird.png");
-}
-
-const VELOCITY = 200;
-
-let bird = null;
-let flapVelocity = 300;
-let totalDelta = null;
-
-function create() {
-	this.add.image(0, 0, "sky").setOrigin(0);
-	bird = this.physics.add
-		.sprite(config.width * 0.1, config.height / 2, "bird")
-		.setOrigin(0);
-
-	this.input.on("pointerdown", flap);
-	this.input.keyboard.on("keydown_SPACE", flap);
-}
-
-// If bird position x is same or larger than width of canvas go back to the left
-// If bird position x is smaller or equal to 0 then move back to the right
-function update(time, delta) {}
-
-function flap() {
-	bird.body.velocity.y = -flapVelocity;
-}
 
 new Phaser.Game(config);
