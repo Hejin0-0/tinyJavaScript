9a50f7329c73faf3afb225c78d3de8c3f3238f28
Hejin0-0 <lovenstein0624@gmail.com>
2024-03-15 14:48:00 +0900

    Image Slicing Effects_2Day-v1

--- ImageEffects/main.js
+++ ImageEffects/main.js
@@ -4,10 +4,15 @@ canvas.width = 500;
 canvas.height = 700;
 
 class Cell {
-	constructor(effect, x, y) {
+	constructor(effect, x, y, index) {
 		this.effect = effect;
 		this.x = x;
 		this.y = y;
+		this.index = index;
+		this.positionX = this.effect.width * 0.5;
+		this.positionY = this.effect.height;
+		this.speedX;
+		this.speedY;
 		this.width = this.effect.cellWidth;
 		this.height = this.effect.cellHeight;
 		this.img = document.getElementById("prImg");
@@ -17,6 +22,10 @@ class Cell {
 		this.vy = 0;
 		this.ease = 0.01;
 		this.friction = 0.9;
+		this.randomize = 5;
+		setTimeout(() => {
+			this.start();
+		}, this.index * 10);
 	}
 
 	draw(context) {
@@ -26,19 +35,39 @@ class Cell {
 			this.y + this.slideY,
 			this.width,
 			this.height,
-			this.x,
-			this.y,
+			this.positionX,
+			this.positionY,
 			this.width,
 			this.height
 		);
-		//context.strokeRect(this.x, this.y, this.width, this.height);
+		context.strokeRect(
+			this.positionX,
+			this.positionY,
+			this.width,
+			this.height
+		);
 	}
+
+	start() {
+		this.speedX = (this.x - this.positionX) / this.randomize;
+		this.speedY = (this.y - this.positionY) / this.randomize;
+	}
+
 	update() {
+		// cell position
+		if (Math.abs(this.speedX) > 0.01 || Math.abs(this.speedX) > 0.01) {
+			this.speedX = (this.x - this.positionX) / this.randomize;
+			this.speedY = (this.y - this.positionY) / this.randomize;
+			this.positionX += this.speedX;
+			this.positionY += this.speedY;
+		}
+
+		// crop
 		const dx = this.effect.mouse.x - this.x;
 		const dy = this.effect.mouse.y - this.y;
 		const distance = Math.hypot(dx, dy);
 		if (distance < this.effect.mouse.radius) {
-			const angle = Map.atan2(dy, dx);
+			const angle = Math.atan2(dy, dx);
 			const force = distance / this.effect.mouse.radius;
 			this.vx = force * Math.cos(angle);
 			this.vy = force * Math.sin(angle);
@@ -53,8 +82,8 @@ class Effect {
 		this.canvas = canvas;
 		this.width = this.canvas.width;
 		this.height = this.canvas.height;
-		this.cellWidth = this.width / 50;
-		this.cellHeight = this.height / 70;
+		this.cellWidth = this.width / 25;
+		this.cellHeight = this.height / 45;
 		this.imgGrid = [];
 		this.createGrid();
 		this.mouse = {
@@ -73,9 +102,11 @@ class Effect {
 	}
 
 	createGrid() {
+		let index = 0;
 		for (let y = 0; y < this.height; y += this.cellHeight) {
-			for (let x = x; x < this.width; x += this.cellWidth) {
-				this.imgGrid.push(new Cell(this, x, y));
+			for (let x = 0; x < this.width; x += this.cellWidth) {
+				index++;
+				this.imgGrid.unshift(new Cell(this, x, y, index));
 			}
 		}
 	}
@@ -91,6 +122,7 @@ class Effect {
 const effect = new Effect(canvas);
 
 function animate() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	effect.render(ctx);
 	requestAnimationFrame(animate);
 }
